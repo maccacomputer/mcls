2eb795d74383bec1dfde44340c0c45db1cf3d8c1
macca <85993132+mcc-macca@users.noreply.github.com>
2022-03-17 19:15:38 +0100

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,10 +1,10 @@
 <?php
 $config = [
-    'db_engine' => 'mysql',
-    'db_host' => 'localhost',
-    'db_name' => 'my_maccacomputer',
-    'db_user' => 'maccacomputer',
-    'db_password' => 'QYxgdnPD3Htd',
+    'db_engine' => '', //example: 'db_engine' => 'mysql',
+    'db_host' => '', // example: 'db_host' => 'localhost',
+    'db_name' => '', //example: 'db_name' => 'my_database',
+    'db_user' => '', //example: 'db_user' => 'paul',
+    'db_password' => '', //example  'db_password' => 'yourpassword',
 ];
 
 $db_config = $config['db_engine'] . ":host=".$config['db_host'] . ";dbname=" . $config['db_name'];
@@ -17,5 +17,5 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
-    exit("Impossibile connettersi al database: " . $e->getMessage());
+    exit("Unable to connect to database: " . $e->getMessage());
 }
